8340e3c897d5d7e40f91b08ed6d8bce69718b444
Naveen Madala <madalanaveen9@gmail.com>
2025-09-08 00:39:33 -0500

    Add Second SQL Solution

--- DataEngineerSQL01.sql
+++ DataEngineerSQL01.sql
@@ -1,8 +1,22 @@
 -- Find Viewers with Multiple Article Views in a Day
 -- https://www.dataexpert.io/question/find-multiple-article-viewers
 
+/* Find Viewers with Multiple Article Views in a Day
+ 
+Using the table playground.views, write a SQL query to identify all viewers who viewed more than one article on the same day. The table includes columns viewer_id (the ID of the viewer), article_id (the ID of the article viewed), and view_date (the date of the view). The result should contain a single column named viewer_id, listing each viewer who meets the criteria without duplicates, and should be sorted in ascending order of viewer_id.
+
+These are the tables to query for this question:
+playground.views
+article_id int
+author_id int
+viewer_id int
+view_date date
+Your answer should include these columns:
+viewer_id integer
+*/
+
 SELECT viewer_id
 FROM playground.views
 GROUP BY view_date, viewer_id
 HAVING COUNT(DISTINCT article_id)>1
-ORDER BY viewer_id
+ORDER BY viewer_id;
--- Readme.md
+++ Readme.md
@@ -1,6 +1,9 @@
 # SQL QA Collections
 
 ## DataExpert.io
-  
-   - Question 01: [Find Viewers with Multiple Article Views in a Day](https://www.dataexpert.io/question/find-multiple-article-viewers)
+
+- Question 01: [Find Viewers with Multiple Article Views in a Day](https://www.dataexpert.io/question/find-multiple-article-viewers)
      - ↪️ [Solution](DataEngineerSQL01.sql)
+
+- Question 02: [Check Test Answers](https://www.dataexpert.io/question/check-test-answers)
+     - ↪️ [Solution](DataEngineerSQL02.sql)
